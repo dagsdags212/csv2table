96cf3d3e21a94b07230dca8289d98089619e8b00
dagsdags212 <jegsamson.dev@gmail.com>
2024-11-20 18:39:26 +0800

    Fixed bug: uncreated files should not be validated

--- csv2table/validation.py
+++ csv2table/validation.py
@@ -5,6 +5,8 @@ def _validate_filepath(filepath: str | Path) -> Path:
     """Returns a Path object if the given file path exists."""
     if isinstance(filepath, str):
         filepath = Path(filepath)
-    if not filepath.exists():
+
+    if not filepath.absolute().exists():
+        print(filepath.absolute())
         raise FileNotFoundError("invalid file path")
     return filepath
